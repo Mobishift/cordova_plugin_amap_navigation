038f96999f32e41fe16437cae621892f3672fe00
Zhengz <zhengz@masn.cn>
2015-05-04 12:41:40 +0800

    init plugin add platform ios and android

---
+++ src/ios/AMapNavigation.m
@@ -0,0 +1,28 @@
+/********* AMapNavigation.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface AMapNavigation : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation AMapNavigation
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
