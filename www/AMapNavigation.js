bc19b391d11ddf7cbecbd08c47a3efdc92a042d5
Zhengz <zhengz@masn.cn>
2015-05-04 17:12:27 +0800

    fix js

--- www/AMapNavigation.js
+++ www/AMapNavigation.js
@@ -1,6 +1,6 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
+exports.exec = function(arg0, success, error) {
     exec(success, error, "AMapNavigation", "coolMethod", [arg0]);
 };
 
